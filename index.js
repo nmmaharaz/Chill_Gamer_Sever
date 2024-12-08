e065505f4e27b2b5d6cbf6bdb1d9ef59696873f6
nmmaharaz <nmmaharaz@gmail.com>
2024-12-08 13:56:24 +0600

    add Review

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ async function run() {
       const result = await mywishlistcollection.insertOne(addReview);
       res.send(result);
     });
-
+// hellow
     app.delete("/mywishlist/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
